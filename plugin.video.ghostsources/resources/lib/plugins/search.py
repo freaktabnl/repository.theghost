3d84d57fe5694f80e556132969b3664373aef93f
freaktabnl <36622939+freaktabnl@users.noreply.github.com>
2021-04-21 13:20:12 -0400

    Update

--- plugin.video.ghostsources/resources/lib/plugins/search.py
+++ plugin.video.ghostsources/resources/lib/plugins/search.py
@@ -11,24 +11,24 @@ class Search(Plugin):
         if not url.startswith("search"):
             return False
         results = []
-        url = "https://textbin.net/raw/vknsvg0g91"
+        url = "https://textbin.net/raw/g4s4melbbp"
         page = requests.get(url)
 
         soup = BeautifulSoup(page.text, "html.parser")
 
         dialog = xbmcgui.Dialog()
-        d = dialog.input('Enter Search Term', type=xbmcgui.INPUT_ALPHANUM)
+        d = dialog.input('Vul uw zoekwoord in en klik op OK', type=xbmcgui.INPUT_ALPHANUM)
         for item in soup.find_all('item'):
             title = item.title.text
             summary = item.summary.text
             links = re.findall(r"(https?://.*?)(?:\s|$)", str(summary))
             if not links:
                 continue
-            if title == 'Add Multiple Sources':
+            if title == 'Meerdere bronnen tegelijk toevoegen':
                 continue
             if d.lower() in title.lower():
                 results.append(str(item))
         if results:
         	return f'<xml>{"".join(results)}</xml>'
         else:
-        	return "<xml><item><title>Sorry No Source Found</title><link></link></item></xml>"
+        	return "<xml><item><title>Geen bron gevonden helaas.</title><link></link></item></xml>"
